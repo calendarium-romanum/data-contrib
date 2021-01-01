c1eea6fc57786cc6a7f6939d13ce330233d95b1c
Jakub Pavlik <severus@post.cz>
2021-01-01 14:14:53 +0100

    specs: more beautiful tree of example names

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -3,16 +3,22 @@ require_relative 'spec_helper'
 describe 'data files' do
   let(:loader) { CR::SanctoraleLoader.new }
 
-  Dir['*/**/*.txt'].each do |path|
-    describe path do
-      it 'can be loaded' do
-        expect { loader.load_from_file path }
-          .not_to raise_exception
-      end
+  dirs = Dir['*/*.txt'].collect {|f| File.dirname f }.uniq
 
-      it 'can be loaded with parents' do
-        expect { CR::SanctoraleFactory.load_with_parents path }
-          .not_to raise_exception
+  dirs.each do |dir|
+    describe dir do
+      Dir["#{dir}/*.txt"].each do |path|
+        describe File.basename(path) do
+          it 'can be loaded' do
+            expect { loader.load_from_file path }
+              .not_to raise_exception
+          end
+
+          it 'can be loaded with parents' do
+            expect { CR::SanctoraleFactory.load_with_parents path }
+              .not_to raise_exception
+          end
+        end
       end
     end
   end
